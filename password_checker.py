4b213f101be01fef18dd0676eb0d29bb020d6b88
altaweelmustafa <afam396@gmail.com>
2026-03-18 15:35:10 +0200

    bug fixed

--- password_checker.py
+++ password_checker.py
@@ -12,8 +12,9 @@ isspecial = False
 # Special char
 special_chars = {'!', '@', '#', '$', '%' , '^', '&', '*', '(' , ')', '-', '_', '+', '=', ';', ':', ',', '.', '"', '~', '`'}
 # Check args
-if len(sys.argv) > 2:
+if len(sys.argv) != 2:
     print(f"{RED}[ERROR]{RESET} Invalid number of args, enter just one password please.")
+    sys.exit(1)
 
 # Save password text
 password = sys.argv[1]
@@ -21,24 +22,24 @@ password = sys.argv[1]
 # Check length
 if len(password) < 8 or len(password) > 26:
     print(f"{RED}[INVALID]{RESET} Length must be between 8 and 26.")
+    sys.exit(1)
 
 # Check capitals and specials
 for ch in password:
     if(ch.isupper()):
         iscapital = True
-        score += 1
     if ch in special_chars:
         isspecial = True
-        score += 1
 
 if not iscapital:
     print(f"{RED}[INVALID]{RESET} No uppercase characters.")
+    sys.exit(1)
 
 if not isspecial:
     print(f"{RED}[INVALID]{RESET} No special characters.")
-    
+    sys.exit(1)
 
-if score >= 2 and iscapital and isspecial and sys.argv[1].isalnum() == False and len(sys.argv) == 2:
+if iscapital and isspecial and sys.argv[1].isalnum() == False and len(sys.argv) == 2:
     print(f"{GREEN}[STRONG]{RESET} Your password is strong.")
 else:
     print(f"{YELLOW}[WEAK]{RESET} Your password is weak.")
